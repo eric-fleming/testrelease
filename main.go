3e2270677c6a62a103cf5316cfe9e73cdd2f5e38
Eric Fleming <eric.fleming@twopoint.com>
2021-04-30 09:53:11 -0500

    feat: square a number

--- main.go
+++ main.go
@@ -4,4 +4,11 @@ import "fmt"
 
 func main() {
 	fmt.Println("Best Program....")
+	fmt.Println("New feature!")
+	n := 10
+	fmt.Printf("%v is a square", square(n))
+}
+
+func square(number int) int {
+	return number * number
 }
